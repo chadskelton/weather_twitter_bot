6812dd98de336272fa3b6a2389b303475c4525f7
Chad Skelton <cskeltonnews@gmail.com>
2019-07-02 13:47:15 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,7 +3,6 @@
 # Was getting 502 errors but then changed to Python 2.7.9 runtime (instead of Python 2.7.6). Seemed to grab the page OK then but had some issues with Tweepy
 
 # @WarmerVancouver
-# Gmail: vancouveriswarmer@gmail.com
 
 import scraperwiki
 import tweepy
